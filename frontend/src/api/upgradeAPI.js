db36220a7674f3eb6875b08d9ce7377828df0885
APpaul2601 <appaul1504@gmail.com>
2025-12-19 11:05:06 -1200

    last commit

--- frontend/src/api/upgradeAPI.js
+++ frontend/src/api/upgradeAPI.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api";
+const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000/api";
 
 const getAuthHeader = () => {
   const token = localStorage.getItem("token");
@@ -8,7 +8,7 @@ const getAuthHeader = () => {
 };
 
 /**
- * @returns {Promise<Array>} 
+ * @returns {Promise<Array>}
  */
 export const getAllUpgrades = async () => {
   try {
@@ -20,25 +20,29 @@ export const getAllUpgrades = async () => {
 };
 
 /**
- * @param {string} upgradeType 
- * @returns {Promise<Object>} 
+ * @param {string} upgradeType
+ * @returns {Promise<Object>}
  */
 
 /**
- * @param {string} upgradeType 
- * @returns {Promise<Object>} 
+ * @param {string} upgradeType
+ * @returns {Promise<Object>}
  */
 export const buyUpgrade = async (upgradeType) => {
   try {
     const headers = { "Content-Type": "application/json", ...getAuthHeader() };
-    const res = await axios.post(`${API_URL}/upgrades/buy`, { upgradeType }, { headers });
-    if (res.data && res.data.data && res.data.data.progress) return res.data.data.progress;
+    const res = await axios.post(
+      `${API_URL}/upgrades/buy`,
+      { upgradeType },
+      { headers }
+    );
+    if (res.data && res.data.data && res.data.data.progress)
+      return res.data.data.progress;
     if (res.data && res.data.progress) return res.data.progress;
     return res.data;
   } catch (err) {
     throw new Error(err.response?.data?.error || "Error buying upgrade");
   }
 };
-
 
 export default { getAllUpgrades, buyUpgrade };
